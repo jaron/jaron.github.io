73218c2fa5528f06df5a783dcf8a14f365ce92e8
Jaron Collis <jaroncollis@gmail.com>
2025-11-17 10:18:17 +0000

    added analytics

---
+++ public/scripts/g-analytics.js
@@ -0,0 +1,6 @@
+// Load GA library
+window.dataLayer = window.dataLayer || [];
+function gtag(){ dataLayer.push(arguments); }
+
+gtag('js', new Date());
+gtag('config', 'G-14NQ6ESSXV'); // replace with your measurement ID
